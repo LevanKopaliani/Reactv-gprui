8bfc6d753b9260f73e130ca8b65f52f2de1178e3
Levan Kopaliani <ciceron23x@gmail.com>
2023-03-27 00:26:43 +0400

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,21 @@ const Home = () => {
   return (
     <div className="homepage">
       <div className="wrapper-p">
+        <div className="homepagetext-container">
+          <h1 className="homepage-title">
+            SAN FRANCISCO
+            <br />
+            <p>
+              <img src={TitleArrow} alt="" />
+              TO HAWAII
+            </p>
+            <p>MAY 2024</p>
+          </h1>
+          <p className="homepage-subtitle">2400 NM OF OCEAN ROWING</p>
+        </div>
         <div className="homepagevideo-container">
           <img src={HomePageVideo} alt="video" />
           <CtaButton className="homepagevideobutton" text="Register Now" />
-        </div>
-        <div className="wrapper">
-          <div className="container">
-            <div className="homepagetext-container">
-              <h1 className="homepage-title">
-                SAN FRANCISCO
-                <br />
-                <p>
-                  <img src={TitleArrow} alt="" />
-                  TO HAWAII
-                </p>
-                <br />
-                MAY 2024
-              </h1>
-            </div>
-          </div>
         </div>
       </div>
     </div>
