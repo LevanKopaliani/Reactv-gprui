b90168c1a43e302ea33b9190e09b7bd1c611b7e8
Levan Kopaliani <ciceron23x@gmail.com>
2023-04-04 21:26:37 +0400

    HomeFinish

--- src/pages/sections/HomeFinish.jsx
+++ src/pages/sections/HomeFinish.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../Styles/HomeFinish.scss";
 import finisher1 from "../../assets/img/finishers1.svg";
 import finisher2 from "../../assets/img/finishers2.svg";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
 
 const HomeFinish = () => {
   const [activeYear, setActiveYear] = useState("2021");
+
+  const swiper = useSwiper();
+
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
 
   return (
     <section className="finish-section">
@@ -26,30 +44,48 @@ const HomeFinish = () => {
             </p>
           </div>
         </div>
-        <div className="finish-section-slider-container container">
-          <div className="finish-section-slider-item">
-            <img src={finisher1} alt="" />
-            <p className="finishers-title">
-              <h2>latitude 35</h2>
-              <p className="record-set">World record set</p>
-            </p>
-            <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
-          </div>
-          <div className="finish-section-slider-item">
-            <img src={finisher2} alt="" />
-            <p className="finishers-title">
-              <h2>Ocean Sheroes</h2>
-              <p className="record-set">World record set</p>
-            </p>
-            <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
-          </div>
-          <div className="finish-section-slider-item">
-            <img src={finisher2} alt="" />
-            <p className="finishers-title">
-              <h2>Ocean Sheroes</h2>
-              <p className="record-set">World record set</p>
-            </p>
-            <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
+        <div className="finish-section-slider-wrapper">
+          <div className="finish-section-slider-container">
+            <Swiper
+              // modules={[Navigation]}
+              spaceBetween={20}
+              slidesPerView={
+                windowSize < 700 ? 1.1 : windowSize < 1020 ? 2 : 2.5
+              }
+              // navigation={{ nextEl: ".nextel", prevEl: ".prevel" }}
+              // centeredSlides={true}
+            >
+              <SwiperSlide>
+                <div className="finish-section-slider-item">
+                  <img src={finisher1} alt="" />
+                  <p className="finishers-title">
+                    <h2>latitude 35</h2>
+                    <p className="record-set">World record set</p>
+                  </p>
+                  <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="finish-section-slider-item">
+                  <img src={finisher2} alt="" />
+                  <p className="finishers-title">
+                    <h2>Ocean Sheroes</h2>
+                    <p className="record-set">World record set</p>
+                  </p>
+                  <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="finish-section-slider-item">
+                  <img src={finisher2} alt="" />
+                  <p className="finishers-title">
+                    <h2>Ocean Sheroes</h2>
+                    <p className="record-set">World record set</p>
+                  </p>
+                  <p className="race-time">30 Days, 5 Hours, 37 Minutes</p>
+                </div>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </div>
